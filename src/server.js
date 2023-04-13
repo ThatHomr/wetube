d7a927e1e8bea06b9ce229a1f7da118e8afabdd2
ThatHomr <34852381a@gmail.com>
2023-04-13 09:02:46 +0900

    23-04-12

--- src/server.js
+++ src/server.js
@@ -13,8 +13,42 @@ const handleHome = (req, res) => {
     return res.send("I love middlewares");
 };
 
-app.get("/", logger, handleHome);
+const URLLogger = (req, res, next) => {
+    console.log(`Path : ${req.path}`);
+    next();
+};
+
+const TimeLogger = (req, res, next) => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+    console.log(`Time: ${year}.${month}.${day}`)
+    next();
+};
+
+const SecurityLogger = (req, res, next) => {
+    const protocol = req.protocol;
+    let col;
+    if (protocol === "https") {
+        col = "secure"
+    } else {
+        col = "insecure"
+    }
+    console.log(`${col}`);
+    next();
+};
+
+const ProtectorMiddleware = (req, res) => {
+    const path = req.path
+    if (path === "/protected"){
+        return res.send("<h1>Protected<h1>")
+    }
+};
+
+app.get("/", logger, URLLogger, TimeLogger, SecurityLogger, handleHome);
+app.get("/protected", ProtectorMiddleware);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
