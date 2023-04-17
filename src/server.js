56ddeb6bbc2a5ff1fe56e72962b2ae284f8d44e8
ThatHomr <34852381a@gmail.com>
2023-04-17 09:05:34 +0900

    23-04-17

--- src/server.js
+++ src/server.js
@@ -1,51 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = logger("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
 
-const handleHome = (req, res) => {
-    return res.send("I love middlewares");
-};
-
-const URLLogger = (req, res, next) => {
-    console.log(`Path : ${req.path}`);
-    next();
-};
-
-const TimeLogger = (req, res, next) => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
-    console.log(`Time: ${year}.${month}.${day}`)
-    next();
-};
-
-const SecurityLogger = (req, res, next) => {
-    const protocol = req.protocol;
-    let col;
-    if (protocol === "https") {
-        col = "secure"
-    } else {
-        col = "insecure"
-    }
-    console.log(`${col}`);
-    next();
-};
-
-const ProtectorMiddleware = (req, res) => {
-    const path = req.path
-    if (path === "/protected"){
-        return res.send("<h1>Protected<h1>")
-    }
-};
-
+app.use(logger)
 app.get("/", logger, URLLogger, TimeLogger, SecurityLogger, handleHome);
 app.get("/protected", ProtectorMiddleware);
 
