2fb7adafbd1d281b31994aff2f45eedb1532fdbc
ThatHomr <34852381a@gmail.com>
2023-04-24 20:46:56 +0900

    23-04-24

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,10 @@
 
-export const trending = (req, res) => {
-    res.render("home")
-};
+export const trending = (req, res) => res.render("home", {pageTitle:"Home"});
 
 export const search = (req, res) => res.send("Search");
 
-export const see = (req, res) => {
-    res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-    console.log(req.params);
-    res.send("Edit Video");
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => {
     console.log(req.params);
     res.send("Delete Video");
